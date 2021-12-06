fa7f5f7eb0b34a1714916780aa52cf7c7bed33ec
Frankigeno <frankigeno@gmail.com>
2021-12-06 18:05:01 +0100

    license

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name = "alkaline",
-    version = "1.1",
+    version = "0.1.1",
     author = "Levorin Francesco",
     description = "Alkaline is a way to render HTML pages with all the features of Python",
     packages = setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: None",
         "Operating System :: OS Independent"
     ],
     py_modules = ["alkaline"],
